a2944848f6997dead233272ab2dfebaf41a39d90
nicosesma <nicosm310@gmail.com>
2017-01-20 11:47:08 -0800

    WIP setting up env config for JWT_PUBLIC_KEY

--- config/config.js
+++ config/config.js
@@ -15,14 +15,25 @@ const testEnv = () => {
   return process.env.NODE_ENV || 'test'
 }
 
+const readEnvFile = () => {
+  if( fs.existsSync( '.env' ) ) {
+    require( 'dotenv' ).config()
+    console.log('process.env', process.env)
+  } else {
+    console.log( ".env not found, skipping dotenv config..." )
+  }
+}
+
 const readConfig = () => {
   if(config){
     return config
   } else {
     const _env = env()
     const filepath = path.join(__dirname, `./${_env}.json`)
+
     try {
       config = JSON.parse(fs.readFileSync(filepath).toString())
+      readEnvFile()
       return config
     } catch(e) {
       throw new Error(`Error reading config file : ${filepath}`)
--- init/auth.js
+++ init/auth.js
@@ -7,12 +7,6 @@ const init = function(expressApp, config) {
     refreshUserFromIDMService
   } = require('@learnersguild/idm-jwt-auth/lib/middlewares')
 
-  //NOTE: need to do this as idm-jwt-auth token needs this env var
-  //Also we dont want to store the production key on github
-  if(!isProduction()) {
-    process.env.JWT_PUBLIC_KEY = config.auth.JWT_PUBLIC_KEY
-  }
-
   expressApp.use(addUserToRequestFromJWT)
   const ensureUserLoggedIn = (req, res, next) => {
     const redirectTo = encodeURIComponent(config.host_fully_qualified)
